c6ed0ea84a674bb1f77563eb683f1dd97246546c
zxccong <1209252967@qq.com>
2019-04-30 17:47:53 +0800

    一点点

--- day1/src/day4/Demo1_ForFor.java
+++ day1/src/day4/Demo1_ForFor.java
@@ -5,11 +5,31 @@ package day4;
  */
 public class Demo1_ForFor {
     public static void main(String[] args) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
+
+        //输出四行五列的星
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        //打印正三角形
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        //九九乘法表
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j+" * "+i+" = "+(j*i)+"\t");
+            }
+            System.out.println();
+        }
+        //windows系统里面/r/n才表示换行/r表示行首/n表示换行
+
     }
 }
