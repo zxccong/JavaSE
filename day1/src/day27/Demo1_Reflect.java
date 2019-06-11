573bcef3add358e2f2637a8ffefef51baa623751
zxccong <1209252967@qq.com>
2019-06-12 06:23:55 +0800

    day27

---
+++ day1/src/day27/Demo1_Reflect.java
@@ -0,0 +1,10 @@
+package day27;
+
+/**
+ * 反射
+ */
+public class Demo1_Reflect {
+    public static void main(String[] args) {
+        Class<?> aClass = Class.forName("day07.Demo_Student");
+    }
+}
